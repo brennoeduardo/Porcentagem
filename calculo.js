7aae9eb40a68ab6953d6ada57bad43d254262afe
Brenno Eduardo <brennobesc@gmail.com>
2022-03-15 22:17:40 -0300

    first commit

---
+++ calculo.js
@@ -0,0 +1,17 @@
+const moeda = document.getElementById("input-numero")
+const porcentagem =document.getElementById("input-porcentagem")
+var resultado = document.getElementById("resultado2")
+const  calcular =document.getElementById("botao")
+
+
+calcular.addEventListener("click", function(){
+
+    if(porcentagem.value == "" || moeda.value == "" && porcentagem.value <= 0 || moeda.value <= 0 ){
+        resultado.style.color = "red"
+        resultado.innerText = "Insira valores válidos!"
+    } else {
+        var calculo = Number(porcentagem.value) * Number(moeda.value) / 100
+        resultado.innerHTML = calculo
+    }
+
+})
